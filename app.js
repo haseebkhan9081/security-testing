b28d85348d5ec6c912056d9c9a757aa8af93fbe0
haseebkhan9081 <aa9081haseeb@gmail.com>
2023-01-11 21:40:33 +0500

    from vscode

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express=require("express");
 const ejs=require("ejs");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
-const encrypt =require("mongoose-encryption");
+const session = require('express-session');
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose"); 
 const app= express();
 
 app.use(express.static("public"));
@@ -15,42 +17,21 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 //creating a DataBase
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
-mongoose.set('strictQuery',true);
+mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser:true});
+
 const userSchema=new mongoose.Schema({
     email:String,
     password:String
 });
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+
 const User=mongoose.model("User",userSchema);
 app.post("/register",function(req,res){
-    const newUser= new User({
-        email:req.body.username,
-        password:req.body.password
-    });
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{res.render("secrets")}
-    });
+     
 
-})
+});
 app.post("/login",function(req,res){
-    const username=req.body.username;
-    const pass=req.body.password;
-User.findOne({email:username},function(err,found){
-    if(err){
-        console.log(err);
-    }else{
-        if(found){
-            if(found.password===pass){
-                res.render("secrets");
-            }
-        }
-    }
-})
-
-
+  
+    
 });
 //==============================end DB================================
 app.get("/",function(req,res){
